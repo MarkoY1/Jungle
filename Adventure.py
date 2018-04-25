569cff9a992cc973ca110cf9a56650f35f42581b
marko.yovanovich <marko.yovanovich@e3civichigh.com>
2018-04-25 14:38:44 -0700

    Work on rooms

--- Adventure.py
+++ Adventure.py
@@ -2,35 +2,38 @@ from Room import Room
 from Inventory import Inventory
 print 'JUNGLE'
 print 'You wake up on a dirt jungle floor.'
-JungleFloor = Room('JungleFloor', 'Start', 'J')
+JungleFloor = Room('', 'Start', 'J')
 Cave = Room('Entrance to Cave', 'You are inside the cave but it is too dark to see.', 'c')
 CliffSide = Room('CliffSide', 'You see the Cliff-side in front of you and can not climb it.', 'cs')
 DenseJungle = Room('Dense Jungle', 'You are in front of a Dense jungle.', 'j')
 Tribe = Room('Tribe', 'You see a tribe that is about a mile away.', 't')
 InsideCave = Room('Inside cave', 'You are inside the cave and you see...', 'ic')
 InsideJungle = Room('Inside Jungle', 'You are inside a wild jungle.', 'ij')
-TallTree = Room('Tall Tree', 'You climb the tall tree and find a torch.', 'tt')
+TallTree = Room('Tall Tree', 'You climb the tall tree and find a torch then climbed down.', 'tt')
 Trail = Room('Trail', 'You start walking down a trail towards the tribe.', 'tr')
+
 
 JungleFloor.add_connection(Cave, "Cave entrance", ["north", "n"])
 Cave.add_connection(JungleFloor, 'trail to the exit', ["south", "s"])
 InsideCave.add_connection(InsideCave, "passage", ["west", "w"])
 
 JungleFloor.add_connection(CliffSide, "CliffSide", ["west", "w"])
-CliffSide.add_connection(JungleFloor, "YES", ["east", "e"])
+CliffSide.add_connection(JungleFloor, "Jungle Floor", ["east", "e"])
 
 JungleFloor.add_connection(DenseJungle, "Dense Jungle", ["south", "s"])
-DenseJungle.add_connection(Cave, "YeS", ["north", "n"])
-DenseJungle.add_connection(InsideJungle, "Wild Jungle you see tall trees all around and one very tall tree to the", ["west", "w"])
-InsideJungle.add_connection(TallTree, "You climb the tree and find a flashlight on top of the tree", ["north", "n"])
+DenseJungle.add_connection(JungleFloor, "Jungle Floor", ["north", "n"])
+DenseJungle.add_connection(TallTree, "a very tall tree", ["west", "w"])
+TallTree.add_connection(DenseJungle, "Dense Jungle", ["east", "e"])
+
+
 
 
 JungleFloor.add_connection(Tribe, "Tribe", ["east", "e"])
-Tribe.add_connection(JungleFloor, "YES", ["west", "w"])
+Tribe.add_connection(JungleFloor, "Jungle Floor", ["west", "w"])
 
-inventory = Inventory()
+Inventory = Inventory()
 current_room = JungleFloor
-current_room.enter_room(inventory)
+current_room.enter_room(Inventory)
 
 while True:
     command = raw_input("what would you like to do?")
@@ -56,7 +59,7 @@ while True:
     elif direction == 'x':
         break
     else:
-        print "Ouch! You ran into a wall."
+        print "You can't go that way"
 
 
         pass
